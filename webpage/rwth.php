c2e772a3e74fc7b71eaec07703d156a678ff4862
Tim Niemueller <tim@niemueller.de>
2003-11-18 12:35:51 +0000

    XHTML 1.1 support and fixes

--- webpage/rwth.php
+++ webpage/rwth.php
@@ -1,6 +1,6 @@
 <?php
 
-/* $Id: rwth.php,v 1.5 2003/10/30 14:09:25 tim Exp $ */
+/* $Id: rwth.php,v 1.6 2003/11/18 12:35:51 tim Exp $ */
 
 $title="niemueller.de - palm software - UniMensa Database for RWTH Aachen";
 $meta_desc="A software to keep mensa plans on your Palm.";
@@ -16,35 +16,28 @@ $dbs=array( "RWTH Aachen (generated every friday for next week)" => "UniMensaDB-
 $version="0.3";
 $basename="UniMensa";
 
-box_begin("Uni Mensa Databases", "Last modified ".date ("F d Y H:i:s.", getlastmod()));
+heading_lastmod();
+heading("Uni Mensa Datenbank für die RWTH Aachen",
+        "Wöchentliche Datenbank (Mensaplan)",
+        "unimensa_big.png", "index.php");
+heading_german();
 ?>
-
-<h5>This page is available in German language only!</h5><br/><br/>
-
-<table border="0" cellpadding="4">
- <tr>
-  <td rowspan="2" valign="top"><a href="index.php"><img src="unimensa_big.png" border="0" alt="Uni Mensa"></a></td>
-  <td><h3>Uni Mensa Datenbank f&uuml;r die RWTH Aachen</h3>
- </tr>
- <tr>
-  <td><h4>W&ouml;chentliche Datenbank (Mensaplan)</h4></td>
- </tr>
-</table>
-
 <div class="maintext">
 <h5>Was ist UniMensa?</h5>
 UniMensa ist ein Programm um Mensa-Pläne auf einen PalmOS-Handheld zu syncen und mitzunehmen.
 
 <h5>Was gibt's hier?</h5>
 Hier gibt's jede Woche die aktuelle Datenbank. Sie wird stets freitags um 23:00 generiert.
-Die Datenbank wird von der <a href="http://www.studentenwerk-aachen.de" target="_new">Seite des Studentenwerks Aachen</a>
+Die Datenbank wird von der <a href="http://www.studentenwerk-aachen.de" rel="external">Seite des Studentenwerks Aachen</a>
 generiert.
-Das Programm wird offiziell <a href="http://www.studentenwerk-aachen.de/essen/speiseplaene.asp" target="_new">unterstützt</a> :-)
+Das Programm wird offiziell <a href="http://www.studentenwerk-aachen.de/essen/speiseplaene.asp" rel="external">unterstützt</a> :-)
 
 <h5>Was wird benötigt?</h5>
+<ul>
 <li>PalmOS Handheld mit PalmOS Version 3.5 oder besser (z.B. Palm III(x|xe|c...), m5xx, m1xx)</li>
 <li>UniMensa (gibt's <a href="<?=$basename?>-<?=$version?>_dist/<?=$basename?>-<?=$version?>-de.prc">hier</a>, weitere Sprachen <a href="index.php">hier</a>.
     Übersetzer gesucht; wer also seine Muttersprache bevorzugt...)</li>
+</ul>
 
 <h5>Kurzanleitung</h5>
 Zuerst UniMensa <a href="<?=$basename?>-<?=$version?>_dist/<?=$basename?>-<?=$version?>-de.prc">hier</a> herunterladen.
@@ -58,11 +51,11 @@ Die bestehende Datenbank wird automatisch überschrieben und muss nicht explizit 
 Nun kann das Programm auf dem Palm Handheld gestartet werden und zeigt den aktuellen Mensaplan an.
 
 <h5>Mailingliste</h5>
-Das <a href="http://www.rz.rwth-aachen.de" target="_new">Rechenzentrum</a> der RWTH Aachen hat freundlicherweise eine
+Das <a href="http://www.rz.rwth-aachen.de" rel="external">Rechenzentrum</a> der RWTH Aachen hat freundlicherweise eine
 Mailingliste eingerichtet. Zu dieser Mailingliste schickt der Datenbankgenerator wöchentlich die aktuelle Datenbank.
 Das mag für manche doch einfacher sein, als jedes Mal auf die Website zu müssen. Auch Announcements über neue Versionen
 von UniMensa wird es dort geben.<br/>
-Interessierte können sich <a href="http://mailman.rwth-aachen.de/mailman/listinfo/unimensa" target="_new">hier</a> anmelden,
+Interessierte können sich <a href="http://mailman.rwth-aachen.de/mailman/listinfo/unimensa" rel="external">hier</a> anmelden,
 abmelden etc.
 
 <h5>Datenbank</h5>
@@ -72,12 +65,5 @@ Datenbank: <a href="UniMensaDB-RWTH.pdb">UniMensaDB-RWTH.pdb</a> (<?=$DB_DATE?>,
 
 </div>
 
-<?php
-box_end();
-
-?>
-
 <? include("$DOCUMENT_ROOT/right.inc.php"); ?>
 <? include("$DOCUMENT_ROOT/footer.inc.php"); ?>
-</body>
-</html>
